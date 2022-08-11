3a6045b2a24109278bf8f0d676ccb1a68403bf91
Oubay chetouani <oubcodes@gmail.com>
2022-08-11 23:42:36 +0100

    fixed everything

--- index.js
+++ index.js
@@ -21,23 +21,39 @@ const findModel = async function (name) {
 };
 
 const replaceTemplate = async function (template, dataObj) {
-  const foo = await dataObj;
   const templateData = await fs.readFile(template, "utf-8");
+
   let output = templateData.replaceAll("{%MODELNAME%}", dataObj.modelName);
   output = output.replaceAll("{%QUANTITY%}", dataObj.quantity);
   output = output.replaceAll("{%SCREENPRICE%}", dataObj.screenPrice);
   return output;
 };
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-type": "text/html" });
-  // console.log(template);
-  // res.end(template);
-  readline.close();
-});
-readline.question(`Which model do you want:\n`, (modelName) => {
+
+const promptInput = function () {
+  readline.question(`Which model do you want:\n`, (modelName) => {
+    findModel(modelName).then((model) => {
+      const template = replaceTemplate(
+        "./templates/productTemplate.html",
+        model
+      ).then((template) => {
+        const server = createServer(template);
+        listenServer(server);
+      });
+    });
+  });
+};
+const createServer = function (htmlTemplate) {
+  const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-type": "text/html" });
+    // console.log(template);
+    res.end(htmlTemplate);
+    readline.close();
+  });
+  return server;
+};
+const listenServer = function (server) {
   server.listen(8000, () => {
     console.log(`Listening on port ${server.address().port}`);
   });
-
-  replaceTemplate("./templates/productTemplate.html", findModel(modelName));
-});
+};
+promptInput();
